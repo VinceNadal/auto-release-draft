f045e9b1ed034feaf6bdc5820142e6bdf473d260
Vincent Angelo Nadal <vnadal@fullscale.io>
2022-08-13 17:29:57 +0800

    Parsing event data

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as event from './event'
+import * as version from './version'
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const tag = event.getCreatedTag()
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    if (tag && version.isSemVer(tag)) {
+      // todo
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('release-url', 'https://example.com')
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
