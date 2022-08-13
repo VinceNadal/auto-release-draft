3d4ef6fd930e88e20198594f9593d8bc0a310dbd
Vincent Angelo Nadal <vnadal@fullscale.io>
2022-08-13 21:33:47 +0800

    Use createReleaseDraft in action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import * as core from '@actions/core'
 import * as event from './event'
 import * as version from './version'
+import * as git from './git'
+import {createReleaseDraft} from './github'
 
 export async function run(): Promise<void> {
   try {
+    const token = core.getInput('repo-token')
     const tag = event.getCreatedTag()
+    let releaseUrl = ''
 
     if (tag && version.isSemVer(tag)) {
-      // todo
+      const changelog = await git.getChangesIntroducedByTag(tag)
+
+      releaseUrl = await createReleaseDraft(tag, token, changelog)
     }
 
-    core.setOutput('release-url', 'https://example.com')
+    core.setOutput('release-url', releaseUrl)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
